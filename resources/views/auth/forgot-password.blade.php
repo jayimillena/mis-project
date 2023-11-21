b193c791e1eeee274d24275cea9db2f0c1dd0934
Jay Millena <jay.millena07@gmail.com>
2023-11-21 16:20:51 +0800

    Added forgot password form

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,46 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+    <div class="main-wrapper login-body">
+        <div class="login-wrapper">
+            <div class="container">
+                <div class="loginbox">
+                    <div class="login-left">
+                        <img class="img-fluid" src="assets/img/login.png" alt="Logo">
+                    </div>
+                    <div class="login-right">
+                        <div class="login-right-wrap">
+                            <h1>Reset Password</h1>
+                            <p class="account-subtitle">Let Us Help You</p>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                            <form method="POST" action="{{ route('password.email') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label>{{ __('Enter your registered email address') }} <span
+                                            class="login-danger">*</span></label>
+                                    <x-text-input id="email" class="form-control" type="email" name="email"
+                                        :value="old('email')" required autofocus />
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    <span class="profile-views"><i class="fas fa-envelope"></i></span>
+                                </div>
+                                <div class="form-group">
+                                    <x-primary-button class="btn btn-primary btn-block">
+                                        {{ __('Email Password Reset Link') }}
+                                    </x-primary-button>
+                                </div>
+                                <div class="form-group mb-0">
+                                    <a href="{{ route('login') }}"><button
+                                            class="btn btn-primary primary-reset btn-block"
+                                            type="button">Login</button></a>
+                                </div>
+                            </form>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </form>
+    </div>
 </x-guest-layout>
