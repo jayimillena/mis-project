4de7e9473a61deadae11580dab0390369dd78459
Jay Millena <jay.millena07@gmail.com>
2023-11-21 17:20:26 +0800

    Added temporary avatar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,120 +1,26 @@
-<x-app-layout>
+s<x-app-layout>
   <div class="page-wrapper">
+    
     <div class="page-header">
       <div class="row">
         <div class="col">
           <ul class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-            <li class="breadcrumb-item active">Books</li>
+            <li class="breadcrumb-item"><a href="{{ route('home.index') }}"></a></li>
           </ul>
         </div>
       </div>
     </div>
     
     <div class="row">
-      <div class="col-lg-3 col-md-4">
-        <div class="compose-btn">
-          <a href="" class="btn btn-primary btn-block">
-            Compose
-          </a>
-        </div>
-        <ul class="inbox-menu">
-          <li class="active">
-            <a href="#"><i class="fas fa-download"></i> Inbox <span class="mail-count">(5)</span></a>
-          </li>
-          <li>
-            <a href="#"><i class="far fa-star"></i> Important</a>
-          </li>
-          <li>
-            <a href="#"><i class="far fa-paper-plane"></i> Sent Mail</a>
-          </li>
-          <li>
-            <a href="#"><i class="far fa-file-alt"></i> Drafts <span class="mail-count">(13)</span></a>
-          </li>
-          <li>
-            <a href="#"><i class="far fa-trash-alt"></i> Trash</a>
-          </li>
-        </ul>
+      <div class="col-lg-1 col-md-2">
       </div>
-      <div class="col-lg-9 col-md-8">
+      <div class="col-lg-11 col-md-10">
         <div class="card">
           <div class="card-body">
             <div class="email-header">
               <div class="row">
                 <div class="col top-action-left">
-                  <div class="float-start">
-                    <div class="btn-group dropdown-action">
-                      <button type="button" class="btn btn-white dropdown-toggle" data-bs-toggle="dropdown">Select
-                        <i class="fas fa-angle-down"></i></button>
-                      <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">All</a>
-                        <a class="dropdown-item" href="#">None</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Read</a>
-                        <a class="dropdown-item" href="#">Unread</a>
-                      </div>
-                    </div>
-                    <div class="btn-group dropdown-action">
-                      <button type="button" class="btn btn-white dropdown-toggle" data-bs-toggle="dropdown">Actions
-                        <i class="fas fa-angle-down"></i></button>
-                      <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Reply</a>
-                        <a class="dropdown-item" href="#">Forward</a>
-                        <a class="dropdown-item" href="#">Archive</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Mark As Read</a>
-                        <a class="dropdown-item" href="#">Mark As Unread</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Delete</a>
-                      </div>
-                    </div>
-                    <div class="btn-group dropdown-action">
-                      <button type="button" class="btn btn-white dropdown-toggle" data-bs-toggle="dropdown"><i
-                          class="fas fa-folder"></i> <i class="fas fa-angle-down"></i></button>
-                      <div role="menu" class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Social</a>
-                        <a class="dropdown-item" href="#">Forums</a>
-                        <a class="dropdown-item" href="#">Updates</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Spam</a>
-                        <a class="dropdown-item" href="#">Trash</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">New</a>
-                      </div>
-                    </div>
-                    <div class="btn-group dropdown-action">
-                      <button type="button" data-bs-toggle="dropdown" class="btn btn-white dropdown-toggle"><i
-                          class="fas fa-tags"></i> <i class="fas fa-angle-down"></i></button>
-                      <div role="menu" class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Work</a>
-                        <a class="dropdown-item" href="#">Family</a>
-                        <a class="dropdown-item" href="#">Social</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Primary</a>
-                        <a class="dropdown-item" href="#">Promotions</a>
-                        <a class="dropdown-item" href="#">Forums</a>
-                      </div>
-                    </div>
-                    <div class="btn-group dropdown-action mail-search">
-                      <input type="text" placeholder="Search Messages" class="form-control search-message">
-                    </div>
-                  </div>
-                </div>
-                <div class="col-auto top-action-right">
-                  <div class="text-end">
-                    <button type="button" title="Refresh" data-bs-toggle="tooltip"
-                      class="btn btn-white d-none d-md-inline-block"><i class="fas fa-sync-alt"></i></button>
-                    <div class="btn-group">
-                      <a class="btn btn-white"><i class="fas fa-angle-left"></i></a>
-                      <a class="btn btn-white"><i class="fas fa-angle-right"></i></a>
-                    </div>
-                  </div>
-                  <div class="text-end">
-                    <span class="text-muted d-none d-md-inline-block">Showing 10 of 112 </span>
-                  </div>
-                </div>
-              </div>
-            </div>
+                  
             <div class="email-content">
               <div class="table-responsive">
                 <table class="table table-inbox table-hover">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,95 +63,8 @@
                                 <a class="dropdown-item" href="javascript:;"><i
                                         class="flag flag-lr me-2"></i>English</a>
                                 <a class="dropdown-item" href="javascript:;"><i
-                                        class="flag flag-bl me-2"></i>Francais</a>
-                                <a class="dropdown-item" href="javascript:;"><i class="flag flag-cn me-2"></i>Turkce</a>
+                                        class="flag flag-ph me-2"></i>Filipino</a>
                             </div>
-                        </div>
-                    </div>
-                </li>
-
-                <li class="nav-item dropdown noti-dropdown me-2">
-                    <a href="#" class="dropdown-toggle nav-link header-nav-list" data-bs-toggle="dropdown">
-                        <img src="assets/img/icons/header-icon-05.svg" alt="">
-                    </a>
-                    <div class="dropdown-menu notifications">
-                        <div class="topnav-dropdown-header">
-                            <span class="notification-title">Notifications</span>
-                            <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
-                        </div>
-                        <div class="noti-content">
-                            <ul class="notification-list">
-                                <li class="notification-message">
-                                    <a href="#">
-                                        <div class="media d-flex">
-                                            <span class="avatar avatar-sm flex-shrink-0">
-                                                <img class="avatar-img rounded-circle" alt="User Image"
-                                                    src="assets/img/profiles/avatar-02.jpg">
-                                            </span>
-                                            <div class="media-body flex-grow-1">
-                                                <p class="noti-details"><span class="noti-title">Carlson Tech</span> has
-                                                    approved <span class="noti-title">your estimate</span></p>
-                                                <p class="noti-time"><span class="notification-time">4 mins ago</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="notification-message">
-                                    <a href="#">
-                                        <div class="media d-flex">
-                                            <span class="avatar avatar-sm flex-shrink-0">
-                                                <img class="avatar-img rounded-circle" alt="User Image"
-                                                    src="assets/img/profiles/avatar-11.jpg">
-                                            </span>
-                                            <div class="media-body flex-grow-1">
-                                                <p class="noti-details"><span class="noti-title">International Software
-                                                        Inc</span> has sent you a invoice in the amount of <span
-                                                        class="noti-title">$218</span></p>
-                                                <p class="noti-time"><span class="notification-time">6 mins ago</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="notification-message">
-                                    <a href="#">
-                                        <div class="media d-flex">
-                                            <span class="avatar avatar-sm flex-shrink-0">
-                                                <img class="avatar-img rounded-circle" alt="User Image"
-                                                    src="assets/img/profiles/avatar-17.jpg">
-                                            </span>
-                                            <div class="media-body flex-grow-1">
-                                                <p class="noti-details"><span class="noti-title">John Hendry</span> sent
-                                                    a cancellation request <span class="noti-title">Apple iPhone
-                                                        XR</span></p>
-                                                <p class="noti-time"><span class="notification-time">8 mins ago</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="notification-message">
-                                    <a href="#">
-                                        <div class="media d-flex">
-                                            <span class="avatar avatar-sm flex-shrink-0">
-                                                <img class="avatar-img rounded-circle" alt="User Image"
-                                                    src="assets/img/profiles/avatar-13.jpg">
-                                            </span>
-                                            <div class="media-body flex-grow-1">
-                                                <p class="noti-details"><span class="noti-title">Mercury Software
-                                                        Inc</span> added a new product <span class="noti-title">Apple
-                                                        MacBook Pro</span></p>
-                                                <p class="noti-time"><span class="notification-time">12 mins ago</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="topnav-dropdown-footer">
-                            <a href="#">View all Notifications</a>
                         </div>
                     </div>
                 </li>
@@ -166,10 +79,12 @@
                     <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
                         <span class="user-img">
                             <img class="rounded-circle" src="assets/img/profiles/avatar-01.jpg" width="31"
-                                alt="Soeng Souy">
+                                alt="Jay">
                             <div class="user-text">
-                                <h6>Soeng Souy</h6>
-                                <p class="text-muted mb-0">Administrator</p>
+                                @auth
+                                    <h6>Jay</h6>
+                                    <p class="text-muted mb-0">{{ __('Book Wizard') }}</p>    
+                                @endauth
                             </div>
                         </span>
                     </a>
@@ -180,12 +95,13 @@
                                     class="avatar-img rounded-circle">
                             </div>
                             <div class="user-text">
-                                <h6>Soeng Souy</h6>
-                                <p class="text-muted mb-0">Administrator</p>
+                                @auth
+                                    <h6>Jay</h6>
+                                    <p class="text-muted mb-0">{{ __('Book Wizard') }}</p>    
+                                @endauth
                             </div>
                         </div>
-                        <a class="dropdown-item" href="profile.html">My Profile</a>
-                        <a class="dropdown-item" href="inbox.html">Inbox</a>
+                        <a class="dropdown-item" href="">{{ __('Profile') }}</a>
                         @auth
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
@@ -202,35 +118,17 @@
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
                         <li class="menu-title">
-                            <span>Main Menu</span>
-                        </li>
-                        <li class="submenu active">
-                            <a href="#"><i class="feather-grid"></i> <span> Dashboard</span> <span
-                                    class="menu-arrow"></span></a>
-                            <ul>
-                                <li><a href="index.html" class="active">Admin Dashboard</a></li>
-                                <li><a href="teacher-dashboard.html">Teacher Dashboard</a></li>
-                                <li><a href="student-dashboard.html">Student Dashboard</a></li>
-                            </ul>
+                            <span>{{ __('Main Menu') }}</span>
                         </li>
                         <li class="submenu">
-                            <a href="#"><i class="fas fa-graduation-cap"></i> <span> Students</span> <span
+                            <a href="#"><i class="fas fa-book-reader"></i> <span>{{ __('Books Available') }}</span> <span
                                     class="menu-arrow"></span></a>
                             <ul>
-                                <li><a href="students.html">Student List</a></li>
-                            </ul>
-                        </li>
-                        <li class="submenu">
-                            <a href="#"><i class="fas fa-book-reader"></i> <span>Books Available</span> <span
-                                    class="menu-arrow"></span></a>
-                            <ul>
-                                <li><a href="subjects.html">Books list</a></li>
-                                <li><a href="add-subject.html">Contribute a resources</a></li>
-                                <li><a href="edit-subject.html">Recommend a books</a></li>
+                                <li><a href="subjects.html">{{ __('Lists') }}</a></li>
                             </ul>
                         </li>
                         <li>
-                            <a href="blank-page.html"><i class="fas fa-file"></i> <span>Blank Page</span></a>
+                            <a href="blank-page.html"><i class="fas fa-file"></i> <span>{{ __('Publish a Book') }}</span></a>
                         </li>
                     </ul>
                 </div>
@@ -242,7 +140,7 @@
         </div>
 
         <footer>
-            <p>Copyright © 2023 CozyBook Hub.</p>
+            <p>{{ __('Copyright © 2023 CozyBook Hub.') }}</p>
         </footer>
     </div>
     </div>
